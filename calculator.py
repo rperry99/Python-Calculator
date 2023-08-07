6b378f3e240b057041d4a4d505843d2c3a675694
rperry99 <rperry1886@gmail.com>
2023-08-07 15:24:40 -0400

    Refactor input to a function

--- calculator.py
+++ calculator.py
@@ -1,11 +1,18 @@
-# Prompt the user, and get their starting equation.
-string_equation = input('Enter an equation, with the format XX + YY. I can add, subtract, multiply, and divide.\n')
+### Function to gather the information for the equation
+def equation_input():
+    # Prompt the user, and get their starting equation.
+    string_equation = input('Enter an equation, with the format XX + YY. I can add, subtract, multiply, and divide.\n')
 
-# Split their equation string into an array. For the numbers, I also convert the string to int here.
-split_equation = string_equation.split()
-num1 = int(split_equation[0])
-operator = split_equation[1]
-num2 = int(split_equation[2])
+    # Split their equation string into an array. For the numbers, I also convert the string to int here.
+    split_equation = string_equation.split()
+    num1 = int(split_equation[0])
+    operator = split_equation[1]
+    num2 = int(split_equation[2])
+
+    # Call the function to do the math
+    do_the_math(num1, num2, operator)
+
+
 
 ### Function to do the operation
 def do_the_math(num1, num2, operator):
@@ -21,7 +28,7 @@ def do_the_math(num1, num2, operator):
         case _:
             print("You have entered an invalid formula. Please try again.")
             return
-    print(string_equation, " = ", final_answer)
-            
-# Call the math function to do the thing.
-do_the_math(num1, num2, operator)
+    print("The answer to", num1, operator, num2, "is", final_answer)
+        
+# Start the program
+equation_input()
